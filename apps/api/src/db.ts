891a44540815455b0090b87499a11660448bf583
itsanla <anlaharpanda@gmail.com>
2026-05-03 18:44:10 +0700

    Remove wrangler configuration file as part of project restructuring

--- apps/api/src/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/d1";
-import * as schema from "./db/schema";
-import type { Env } from "./types";
-
-export function getDb(env: Env) {
-  return drizzle(env.DB, { schema });
-}
-
-export type DB = ReturnType<typeof getDb>;
